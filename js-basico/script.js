3a5c219d134ea47b3b94b8a7854262bd1c672b50
luanna-n <nayara.luanna@hotmail.com>
2021-07-12 16:07:15 -0300

    orientação a objeto em js

---
+++ js-basico/script.js
@@ -0,0 +1,65 @@
+/*
+    class Book {
+        constructor(title, author, pages) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+        }
+
+        read() {
+            return `Estou lendo ${this.title}`
+        }
+    }
+
+    let book = new Book("Algoritmos para viver", "Brian", 500);
+    let book2 = new Book("Um exu em NY", "Cidinha da Silva", 100);
+
+    console.log(book.read());
+    console.log(book2);
+*/
+
+
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    read() {
+        return `Estou lendo ${this.title}`
+    }
+}
+
+//HERANÇA
+class ITBook extends Book {
+    constructor(title, author, pages, tech) {
+        super(title, author, pages);
+        this.tech = tech;
+    }
+}
+
+let itBook = new ITBook("Algoritmos para viver", "Brian", 500, "Algoritmos");
+
+console.log(itBook.read());
+console.log(itBook);
+
+
+//ENCAPSULAMENTO
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() { //pega esse nome joga lá no console person.name
+        return this._name;
+    }
+
+    set name(value) { //permite reatribuir um valor
+        this._name = value;
+    }
+}
+
+let person = new Person("Maria");
+person.name = "João";
+console.log(person.name);
